aa7831ab0007c185e5785826d215784d2cb744bf
Jhoan Montes <118642633+JhoanMontes@users.noreply.github.com>
2025-08-28 14:05:24 -0500

    Update upload.js

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,10 +2,8 @@ import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
 
-
 const uploadDir = process.env.UPLOAD_DIR || 'uploads';
 if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-
 
 const storage = multer.diskStorage({
 destination: (req, file, cb) => cb(null, uploadDir),
@@ -15,6 +13,5 @@ const name = `${Date.now()}-${Math.round(Math.random()*1e9)}${ext}`;
 cb(null, name);
 }
 });
-
 
 export const upload = multer({ storage });
